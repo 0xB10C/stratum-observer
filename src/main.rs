98d13c5c0c19b74a16683614552e535d08b811bc
0xb10c <b10c@b10c.me>
2024-05-17 15:48:31 +0200

    drop: terminal visualization
    The web one works quite well, no need to keep a terminal one
    around

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use crate::schema::job_updates;
 use crate::types::JobUpdate;
 use crate::types::JobUpdateJson;
 use crate::types::NewJobUpdate;
-use crate::utils::{bip34_coinbase_block_height, extract_coinbase_string};
 use async_broadcast::broadcast;
 use async_channel::{unbounded, Receiver};
 use async_std::task;
@@ -11,8 +10,7 @@ use diesel::prelude::*;
 use diesel::sqlite::SqliteConnection;
 use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
 use env_logger::Env;
-use log::{debug, error, warn};
-use std::collections::BTreeMap;
+use log::{debug, warn};
 use std::net::TcpListener;
 use tungstenite::accept;
 
@@ -44,11 +42,6 @@ fn main() {
         });
     }
 
-    let (visualization_sender, visulization_receiver) = unbounded();
-    task::spawn(async move {
-        terminal_visualization_task(visulization_receiver).await;
-    });
-
     let (sqlite_sender, sqlite_receiver) = unbounded();
     task::spawn(async move {
         sqlite_writer_task(sqlite_receiver).await;
@@ -62,7 +55,6 @@ fn main() {
     task::block_on(async {
         loop {
             let job = job_receiver.recv().await.unwrap();
-            //visualization_sender.send(job.clone()).await.unwrap();
             sqlite_sender.send(job.clone()).await.unwrap();
             websocket_sender.send(job).await.unwrap();
         }
@@ -143,62 +135,4 @@ async fn sqlite_writer_task(receiver: Receiver<JobUpdate<'_>>) {
             .execute(&mut conn)
             .expect("Error saving new job update");
     }
-}
-
-async fn terminal_visualization_task(receiver: Receiver<JobUpdate<'_>>) {
-    let mut last_job: BTreeMap<String, JobUpdate> = BTreeMap::new();
-    loop {
-        let job = receiver.recv().await.unwrap();
-        last_job.insert(job.pool.clone().name, job);
-
-        let mut lines: Vec<(u64, String)> = vec![];
-        for (_, j) in last_job.iter() {
-            let template_colors: String = j
-                .job
-                .merkle_branch
-                .iter()
-                .map(|branch| colored(branch.as_ref()[1] % 255, "  "))
-                .collect::<Vec<String>>()
-                .join("");
-            let cb = j.clone().coinbase();
-            let coinbase_script_sig = &cb
-                .input
-                .first()
-                .expect("coinbase should have one input")
-                .script_sig;
-            let prevhash = j.prev_block_hash();
-            let prevhash_colored = colored(prevhash[0], &prevhash.to_string());
-            let output_value_f64: f64 = cb.output.iter().map(|o| o.value.to_btc()).sum::<f64>();
-            let output_value_u64: u64 = cb.output.iter().map(|o| o.value.to_sat()).sum::<u64>();
-            lines.push((
-                output_value_u64,
-                format!(
-                    "{: <18} {:<50} {} {} {} {:>4}s {:>2}s {:>5} {}\t{}",
-                    j.pool.name,
-                    extract_coinbase_string(coinbase_script_sig),
-                    prevhash_colored,
-                    bip34_coinbase_block_height(&coinbase_script_sig).unwrap_or_default(),
-                    colored(
-                        (output_value_u64 % 128 as u64) as u8 + 42u8,
-                        &format!("{:2.8} BTC", output_value_f64),
-                    ),
-                    j.time_connected_seconds(),
-                    j.age(),
-                    j.job.clean_jobs,
-                    template_colors,
-                    j.pool.name,
-                ),
-            ));
-
-            lines.sort_by_key(|(output_value, _)| *output_value);
-            for line in lines.iter().rev() {
-                println!("{}", line.1);
-            }
-            println!("\n\n");
-        }
-    }
-}
-
-fn colored(color: u8, text: &str) -> String {
-    return format!("\x1B[48;5;{}m\x1B[38;5;0m{}\x1B[0m", color, text);
 }
